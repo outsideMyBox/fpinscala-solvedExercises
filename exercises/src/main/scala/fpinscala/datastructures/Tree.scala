b79c48d19f137acb78bb6c782cf7bc188a48347e
franck <amail4-franckgithub@yahoo.fr>
2015-05-10 17:26:52 +0200

    Up to 3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,26 +7,45 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 object Tree {
 
   // Exercise 3.25: size.
-  def size[A](t: Tree[A]): Int = ???
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_)      => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
   // Exercise 3.26: maximum.
-  def maximum(t: Tree[Int]): Int = ???
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v)      => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
   // Exercise 3.27: depth.
-  def depth[A](t: Tree[A]): Int = ???
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(v)      => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
   // Exercise 3.28: map.
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v)      => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
   // Exercise 3.29: size, maximum, depth and map with fold.
-  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = ???
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v)      => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
 
-  def sizeViaFold[A](t: Tree[A]): Int = ???
+  def sizeViaFold[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)(1 + _ + _)
 
-  def maximumViaFold(t: Tree[Int]): Int = ???
-  
-  def depthViaFold[A](t: Tree[A]): Int = ???
-  
-  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def maximumViaFold(t: Tree[Int]): Int =
+    fold(t)(a => a)(_ max _)
+
+  def depthViaFold[A](t: Tree[A]): Int =
+    fold(t)(_ => 0)((a1, a2) => (a1 max a2) + 1)
+
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)((a: A) => Leaf(f(a)): Tree[B])(Branch(_, _))
 
 }
