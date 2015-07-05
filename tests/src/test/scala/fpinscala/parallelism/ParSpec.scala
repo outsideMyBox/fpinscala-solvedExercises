f76cb8c83832612edfc6a2a55438c4270925b53e
franck <amail4-franckgithub@yahoo.fr>
2015-07-05 21:51:43 +0200

    Tests updated up to 7.14

--- tests/src/test/scala/fpinscala/parallelism/ParSpec.scala
+++ tests/src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -6,10 +6,11 @@ import org.specs2.matcher.DataTables
 import org.specs2.runner.JUnitRunner
 import org.junit.runner.RunWith
 import java.util.concurrent._
+import org.specs2.matcher.Matcher
 
 @RunWith(classOf[JUnitRunner])
 class ParSpec extends Specification with DataTables {
-  // Run the tests sequentially.
+  // Run the tests sequentially as these tests assert also the time it takes to run them.
   sequential
 
   //val maxNbOfThreadsInTests = 8
@@ -22,29 +23,45 @@ class ParSpec extends Specification with DataTables {
   case class CB(val b: String) extends ConcreteType(b)
   case class CC(val c: String) extends ConcreteType(c)
 
-  case class CallableCA(durationMillis: Int, value: String = "") extends Callable[CA] {
+  case class CallableCA(durationMillis: Long, value: String = "") extends Callable[CA] {
     def call(): CA = {
       Thread.sleep(durationMillis)
       CA(s"${value}${durationMillis}ms")
     }
   }
 
-  case class CallableCB(durationMillis: Int, value: String = "") extends Callable[CB] {
+  case class CallableCB(durationMillis: Long, value: String = "") extends Callable[CB] {
     def call(): CB = {
       Thread.sleep(durationMillis)
       CB(s"${value}${durationMillis}ms")
     }
   }
 
+  // Predefined durations
+  val durationPrecision = 0.1 // 10%. 
+  val d1 = 100L
+  val d2 = d1 * 2
+  val d3 = d2 * 2
+  val tooLongDuration = d3 * 1000000 // For threads that are not supposed to run. 
+
+  // Test if a duration is in an interval.
+  def be_around(duration: Long, dp: Double = durationPrecision): Matcher[Long] = {
+    val i = (duration * dp).toLong
+    be_>(duration - i) and be_<(duration + i)
+  }
+
+  def safeMaxFor(duration: Long) = (duration * (1 + durationPrecision)).toLong
+  def safeMinFor(duration: Long) = (duration * (1 - durationPrecision)).toLong
+
   "The following exercises should be correct" >> {
 
     "Exercise 7.3: map2 that respects the contract of timeouts on Future." in {
       "callableA duration" || "callableB duration" || "Wait until" || "exception thrown" |
-        50 !! 100 !! 125 !! false | // 125 as the execution time should be roughly under 150ms but above 100ms.   
-        100 !! 50 !! 125 !! false |
-        150 !! 150 !! 225 !! false | // 125 as the execution time should be roughly under 300ms but above 150ms.
-        190 !! 50 !! 180 !! true | // 190 > 190 so to throw an exception. 
-        50 !! 190 !! 180 !! true |> {
+        d1 !! d2 !! safeMaxFor(d2) !! false |
+        d2 !! d1 !! safeMaxFor(d2) !! false |
+        d3 !! d2 !! safeMaxFor(d3) !! false |
+        d3 !! d2 !! safeMaxFor(d2) !! true | // safeMaxFor(d2) < safeMinFor(d3)
+        d2 !! d3 !! safeMaxFor(d2) !! true |> {
           (durationA, durationB, waitingDuration, exceptionToBeThrown) =>
             def parA(es: ExecutorService): Future[CA] = es.submit(CallableCA(durationA))
             def parB(es: ExecutorService): Future[CB] = es.submit(CallableCB(durationB))
@@ -62,6 +79,7 @@ class ParSpec extends Specification with DataTables {
             }
         }
     }
+
     "Exercise 7.4: asyncF." in {
       def f(a: CA) = CB(a.a)
       val par = Par.asyncF(f)
@@ -69,16 +87,16 @@ class ParSpec extends Specification with DataTables {
     }
 
     "Exercise 7.5: sequence." in {
-      val parA = (es: ExecutorService) => es.submit(CallableCA(50, "1-"))
-      val parB = (es: ExecutorService) => es.submit(CallableCA(50, "2-"))
-      val parC = (es: ExecutorService) => es.submit(CallableCA(50, "3-"))
+      val parA = (es: ExecutorService) => es.submit(CallableCA(d1, "1-"))
+      val parB = (es: ExecutorService) => es.submit(CallableCA(d1, "2-"))
+      val parC = (es: ExecutorService) => es.submit(CallableCA(d1, "3-"))
       val ps = List(parA, parB, parC)
       val parList = Par.sequence(ps)
 
       try {
         // The execution time must be roughly lower than 100ms.
-        val res = parList(es).get(90, TimeUnit.MILLISECONDS)
-        res must_== List(CA("1-50ms"), CA("2-50ms"), CA("3-50ms"))
+        val res = parList(es).get(safeMaxFor(d1), TimeUnit.MILLISECONDS)
+        res must_== List(CA(s"1-${d1}ms"), CA(s"2-${d1}ms"), CA(s"3-${d1}ms"))
       } catch {
         case ex: Throwable => ko(ex.getMessage())
       }
@@ -90,13 +108,12 @@ class ParSpec extends Specification with DataTables {
     // of the newFixedThreadPool but I don't get the exact reason.
     val as = List(CA("1"), CA("2"), CA("3"), CA("4"), CA("5"), CA("6"), CA("7"), CA("8"))
     val parList = Par.parFilter(as) { ca =>
-      Thread.sleep(50)
+      Thread.sleep(d2)
       ca.a == "1" || ca.a == "8"
     }
 
     try {
-      // The execution time must be roughly lower than 2x50ms.
-      val res = parList(es).get(90, TimeUnit.MILLISECONDS)
+      val res = parList(es).get(safeMaxFor(d2), TimeUnit.MILLISECONDS)
       res must_== List(CA("1"), CA("8"))
     } catch {
       case ex: Throwable => ko(ex.getMessage())
@@ -105,34 +122,32 @@ class ParSpec extends Specification with DataTables {
 
   "Exercise 7.11: choiceN." in {
     val callableN = new Callable[Int] {
-      def call(): Int = { Thread.sleep(200); 1 }
+      def call(): Int = { Thread.sleep(d1); 1 }
     }
 
     val n = (es: ExecutorService) => es.submit(callableN)
-    val parA = (es: ExecutorService) => es.submit(CallableCA(5000, "A-"))
-    val parB = (es: ExecutorService) => es.submit(CallableCA(50, "B-"))
-    val parC = (es: ExecutorService) => es.submit(CallableCA(5000, "C-"))
-    val parD = (es: ExecutorService) => es.submit(CallableCA(50, "D-"))
+    val parA = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "A-"))
+    val parB = (es: ExecutorService) => es.submit(CallableCA(d2, "B-"))
+    val parC = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "C-"))
+    val parD = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "D-"))
 
     val t0 = System.currentTimeMillis()
     val parChoiceN = Par.choiceN(n)(List(parA, parB, parC, parD))
     val res = parChoiceN(es).get
     val t1 = System.currentTimeMillis()
     val duration = t1 - t0
-    (res must_== CA("B-50ms")) and
-      (duration must be_<(1000L)) and // Must be roughly < 5000ms.
-      (duration must be_>(225L)) // Must be roughly > 200ms  (callableN + parB).
+    (res must_== CA(s"B-${d2}ms")) and (duration must be_around(d1 + d2))
   }
 
   "Exercise 7.11: choice and terms of choiceN." in {
     val callableChoice = new Callable[Boolean] {
       def call(): Boolean = {
-        Thread.sleep(200)
+        Thread.sleep(d1)
         true
       }
     }
-    val par1 = (es: ExecutorService) => es.submit(CallableCA(5000, "C-"))
-    val par2 = (es: ExecutorService) => es.submit(CallableCA(50, "D-"))
+    val par1 = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "C-"))
+    val par2 = (es: ExecutorService) => es.submit(CallableCA(d2, "D-"))
     val cond = (es: ExecutorService) => es.submit(callableChoice)
 
     val t0 = System.currentTimeMillis()
@@ -140,90 +155,121 @@ class ParSpec extends Specification with DataTables {
     val res = parChoice(es).get
     val t1 = System.currentTimeMillis()
     val duration = t1 - t0
-    (res must_== CA("D-50ms")) and
-      (duration must be_<(1000L)) and // Must be roughly < 5000ms.
-      (duration must be_>(225L)) // Must be roughly > 200ms (callableChoice + par2).
+    (res must_== CA(s"D-${d2}ms")) and (duration must be_around(d1 + d2))
   }
 
-  "Exercise 7.13: chooser." in skipped {
-    val callableN = new Callable[Int] {
-      def call(): Int = {
-        Thread.sleep(10)
-        1
+  "Exercise 7.13: chooser." in {
+    import Par._
+    val callableChooser = new Callable[CA] {
+      def call(): CA = {
+        Thread.sleep(d2)
+        CA("a-")
       }
     }
+
+    def choices(ca: CA): Par[CB] = (es: ExecutorService) => es.submit(CallableCB(d3, ca.a))
+    val pa = (es: ExecutorService) => es.submit(callableChooser)
+
+    val t0 = System.currentTimeMillis()
+    val parChooser = Par.chooser(pa)(choices)
+    val res = parChooser(es).get
+    val t1 = System.currentTimeMillis()
+    (res must_== CB(s"a-${d3}ms")) and ((t1 - t0) must be_around(d2 + d3))
+  }
+
+  "Exercise 7.13: choiceNViaChooser." in {
+    val callableN = new Callable[Int] {
+      def call(): Int = { Thread.sleep(d1); 1 }
+    }
+
+    val n = (es: ExecutorService) => es.submit(callableN)
+    val parA = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "A-"))
+    val parB = (es: ExecutorService) => es.submit(CallableCA(d2, "B-"))
+    val parC = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "C-"))
+    val parD = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "D-"))
+
+    val t0 = System.currentTimeMillis()
+    val parChoiceN = Par.choiceNViaChooser(n)(List(parA, parB, parC, parD))
+    val res = parChoiceN(es).get
+    val t1 = System.currentTimeMillis()
+    val duration = t1 - t0
+    (res must_== CA(s"B-${d2}ms")) and (duration must be_around(d1 + d2))
+  }
+
+  "Exercise 7.13: choiceViaChooser." in {
     val callableChoice = new Callable[Boolean] {
       def call(): Boolean = {
-        Thread.sleep(20)
+        Thread.sleep(d1)
         true
       }
     }
-    val n = (es: ExecutorService) => es.submit(callableN)
-    val parChoice = (es: ExecutorService) => es.submit(callableChoice)
-    val parA = (es: ExecutorService) => es.submit(CallableCA(5000, "A-"))
-    val parB = (es: ExecutorService) => es.submit(CallableCA(50, "B-"))
-    val parC = (es: ExecutorService) => es.submit(CallableCA(5000, "C-"))
-    val parD = (es: ExecutorService) => es.submit(CallableCA(50, "D-"))
+    val par1 = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "C-"))
+    val par2 = (es: ExecutorService) => es.submit(CallableCA(d3, "D-"))
+    val cond = (es: ExecutorService) => es.submit(callableChoice)
 
     val t0 = System.currentTimeMillis()
-    val res0 = Par.choiceNViaChooser(n)(List(parA, parB, parC, parD))
-    val res1 = Par.choiceViaChooser(parChoice)(parD, parC)
+    val parChoice = Par.choiceViaChooser(cond)(par2, par1)
+    val res = parChoice(es).get
     val t1 = System.currentTimeMillis()
-    (res0(es).get must_== CA("B-50ms")) and
-      (res1(es).get must_== CA("D-50ms")) and
-      ((t1 - t0) must be_<(5000L))
+    val duration = t1 - t0
+    (res must_== CA(s"D-${d3}ms")) and (duration must be_around(d1 + d3))
   }
 
-  "Exercise 7.14: join." in skipped {
+  "Exercise 7.14: join." in {
     import Par._
     val callablePar = new Callable[Par[CA]] {
       def call(): Par[CA] = {
-        Thread.sleep(10)
-        val parA = (es: ExecutorService) => es.submit(CallableCA(10, "A-"))
+        Thread.sleep(d2)
+        val parA = (es: ExecutorService) => es.submit(CallableCA(d3, "A-"))
         parA
       }
     }
     val parPar = (es: ExecutorService) => es.submit(callablePar)
-    join(parPar)(es).get must_== CA("A-10ms")
+    val t0 = System.currentTimeMillis()
+    val parJoin = join(parPar)
+    val res = parJoin(es).get
+    val t1 = System.currentTimeMillis()
+    val duration = t1 - t0
+    (res must_== CA(s"A-${d3}ms")) and (duration must be_around(d2 + d3))
   }
 
-  "Exercise 7.14: joinViaFlatMap." in skipped {
+  "Exercise 7.14: joinViaFlatMap." in {
     import Par._
     val callablePar = new Callable[Par[CA]] {
       def call(): Par[CA] = {
-        Thread.sleep(10)
-        val parA = (es: ExecutorService) => es.submit(CallableCA(10, "A-"))
+        Thread.sleep(d1)
+        val parA = (es: ExecutorService) => es.submit(CallableCA(d2, "A-"))
         parA
       }
     }
     val parPar = (es: ExecutorService) => es.submit(callablePar)
-    joinViaFlatMap(parPar)(es).get must_== CA("A-10ms")
+    val t0 = System.currentTimeMillis()
+    val parJoin = joinViaFlatMap(parPar)
+    val res = parJoin(es).get
+    val t1 = System.currentTimeMillis()
+    val duration = t1 - t0
+    (res must_== CA(s"A-${d2}ms")) and (duration must be_around(d1 + d2))
+
   }
 
-  "Exercise 7.14: flatMapViaJoin." in skipped {
+  "Exercise 7.14: flatMapViaJoin." in {
     val callableN = new Callable[Int] {
       def call(): Int = {
-        Thread.sleep(10)
+        Thread.sleep(d1)
         1
       }
     }
-    val callableChoice = new Callable[Boolean] {
-      def call(): Boolean = {
-        Thread.sleep(20)
-        true
-      }
-    }
     val n = (es: ExecutorService) => es.submit(callableN)
-    val parChoice = (es: ExecutorService) => es.submit(callableChoice)
-    val parA = (es: ExecutorService) => es.submit(CallableCA(5000, "A-"))
-    val parB = (es: ExecutorService) => es.submit(CallableCA(50, "B-"))
-    val parC = (es: ExecutorService) => es.submit(CallableCA(5000, "C-"))
-    val parD = (es: ExecutorService) => es.submit(CallableCA(50, "D-"))
+    val parA = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "A-"))
+    val parB = (es: ExecutorService) => es.submit(CallableCA(d2, "B-"))
+    val parC = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "C-"))
+    val parD = (es: ExecutorService) => es.submit(CallableCA(tooLongDuration, "D-"))
 
     val t0 = System.currentTimeMillis()
-    val res = Par.flatMapViaJoin(n)(List(parA, parB, parC, parD))
+    val parFlatMap = Par.flatMapViaJoin(n)(List(parA, parB, parC, parD))
+    val res = parFlatMap(es).get
     val t1 = System.currentTimeMillis()
-    (res(es).get must_== CA("B-50ms")) and ((t1 - t0) must be_<(5000L))
+    (res must_== CA(s"B-${d2}ms")) and ((t1 - t0) must be_around(d1 + d2))
   }
 
 }
